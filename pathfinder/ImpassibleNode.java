b9784b7893e38d51039eb8695d7010d5640b471c
umutarda <52600067+umutarda@users.noreply.github.com>
2022-04-20 13:37:07 +0300

    Add files via upload

---
+++ pathfinder/ImpassibleNode.java
@@ -0,0 +1,16 @@
+package pathfinder;
+import java.awt.Color;
+import java.awt.Point;
+public class ImpassibleNode extends Node {
+    
+    public ImpassibleNode(Point position) 
+    {
+        super(position);
+    }
+
+    @Override
+    public Color getColor() {
+        return Color.GRAY;
+    }
+
+}
