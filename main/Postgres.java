87f26074d772cd8ed9ae0b1d123e4237d07e3879
Umut Can Bolat <umutcanbolat03@gmail.com>
2022-05-05 14:52:46 +0300

    getting data from SQL database
    postgresql 42.3.4 is a dependency, in order to use the sql add it to the class path.

---
+++ main/Postgres.java
@@ -0,0 +1,38 @@
+package main;
+import java.sql.*;
+
+public class Postgres {
+
+    public static void main(String[] args) {
+        try {
+            Class.forName("org.postgresql.Driver");
+        }
+        catch (java.lang.ClassNotFoundException e) {
+            System.out.println(e.getMessage());
+        }
+
+        String url = "jdbc:postgresql://hattie.db.elephantsql.com:5432/gqrbhzwp";
+        String username = "gqrbhzwp";
+        String password = "0ZaNcWqdxpFDgzlB7qvEuJM0ecXkepu4";
+
+        try {
+            Connection db = DriverManager.getConnection(url, username, password);
+            Statement st = db.createStatement();
+            //st.executeQuery ("create table elephant (id int, name varchar)");
+            ResultSet rs = st.executeQuery("SELECT * FROM event_users");
+            while (rs.next()) {
+                
+                System.out.print("Column 1 returned ");
+                System.out.println(rs.getString(1));
+                System.out.print("Column 2 returned ");
+                System.out.println(rs.getString(2));
+            }
+            rs.close();
+            st.close();
+            }
+        catch (java.sql.SQLException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+}
