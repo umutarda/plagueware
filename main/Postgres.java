ce0b3dcdf9849a64e1727e2ee046ce939971becd
umutarda <52600067+umutarda@users.noreply.github.com>
2022-05-06 17:16:41 +0300

    main is commented

--- main/Postgres.java
+++ main/Postgres.java
@@ -3,7 +3,7 @@ import java.sql.*;
 
 public class Postgres {
 
-    public static void main(String[] args) {
+    /*public static void main(String[] args) {
         try {
             Class.forName("org.postgresql.Driver");
         }
@@ -33,6 +33,6 @@ public class Postgres {
         catch (java.sql.SQLException e) {
             System.out.println(e.getMessage());
         }
-    }
+    }*/
 
 }
